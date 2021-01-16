6e0e2b8c4631bad6d5b2ef913a97bc2ddb6d8556
Andre Leiradella <andre@leiradella.com>
2021-01-16 13:16:34 +0000

    Added Lua bindings to Memory; small fixes

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -3,20 +3,63 @@
 #include <imguial_button.h>
 #include <IconsFontAwesome4.h>
 
-bool hc::Memory::init(Logger* logger) {
+#include <inttypes.h>
+
+#define TAG "[MEM] "
+
+hc::Memory::Region::Region(std::string&& name, void* data, size_t offset, size_t size, size_t base, bool readOnly)
+    : name(std::move(name))
+    , data(data)
+    , offset(offset)
+    , size(size)
+    , base(base)
+    , readOnly(readOnly)
+{}
+
+hc::Memory::View::View(Region const& region) : region(region) {
+    editor.OptUpperCaseHex = false;
+    editor.ReadOnly = region.readOnly;
+}
+
+void hc::Memory::init(Logger* const logger) {
     _logger = logger;
-    return true;
 }
 
-void hc::Memory::destroy() {}
-
-void hc::Memory::reset() {
-    _selected = 0;
-    _views.clear();
-    _regions.clear();
+char const* hc::Memory::getName() {
+    return "hc::Memory built-in memory plugin";
 }
 
-void hc::Memory::draw() {
+char const* hc::Memory::getVersion() {
+    return "0.0.0";
+}
+
+char const* hc::Memory::getLicense() {
+    return "MIT";
+}
+
+char const* hc::Memory::getCopyright() {
+    return "Copyright (c) Andre Leiradella";
+}
+
+char const* hc::Memory::getUrl() {
+    return "https://github.com/leiradel/hackable-console";
+}
+
+void hc::Memory::onStarted() {}
+
+void hc::Memory::onConsoleLoaded() {}
+
+void hc::Memory::onGameLoaded() {}
+
+void hc::Memory::onGamePaused() {}
+
+void hc::Memory::onGameResumed() {}
+
+void hc::Memory::onGameReset() {}
+
+void hc::Memory::onFrame() {}
+
+void hc::Memory::onDraw() {
     static auto const getter = [](void* const data, int const idx, char const** const text) -> bool {
         auto const regions = (std::vector<Region>*)data;
 
@@ -51,7 +94,8 @@ void hc::Memory::draw() {
         bool open = true;
 
         if (ImGui::Begin(label, &open)) {
-            view.editor.DrawContents(view.region.data, view.region.size, view.region.base);
+            auto const data = static_cast<uint8_t*>(view.region.data) + view.region.offset;
+            view.editor.DrawContents(data, view.region.size, view.region.base);
         }
 
         if (open) {
@@ -65,19 +109,66 @@ void hc::Memory::draw() {
     }
 }
 
-void hc::Memory::addRegion(char const* const name, void* data, size_t const base, size_t const size, bool const readOnly) {
-    _regions.emplace_back(name, data, base, size, readOnly);
+void hc::Memory::onGameUnloaded() {
+    _selected = 0;
+    _views.clear();
+    _regions.clear();
 }
 
-hc::Memory::Region::Region(char const* name, void* data, size_t base, size_t size, bool readOnly)
-    : name(name)
-    , data(data)
-    , base(base)
-    , size(size)
-    , readOnly(readOnly)
-{}
+void hc::Memory::onConsoleUnloaded() {}
 
-hc::Memory::View::View(Region const& region) : region(region) {
-    editor.OptUpperCaseHex = false;
-    editor.ReadOnly = region.readOnly;
+void hc::Memory::onQuit() {}
+
+int hc::Memory::push(lua_State* const L) {
+    auto const self = static_cast<Memory**>(lua_newuserdata(L, sizeof(Memory*)));
+    *self = this;
+
+    if (luaL_newmetatable(L, "hc::Memory")) {
+        static luaL_Reg const methods[] = {
+            {"addRegion", l_addRegion},
+            {nullptr, nullptr}
+        };
+
+        luaL_newlib(L, methods);
+        lua_setfield(L, -2, "__index");
+    }
+
+    lua_setmetatable(L, -2);
+    return 1;
+}
+
+hc::Memory* hc::Memory::check(lua_State* const L, int const index) {
+    return *static_cast<Memory**>(luaL_checkudata(L, index, "hc::Memory"));
+}
+
+int hc::Memory::l_addRegion(lua_State* const L) {
+    auto const self = check(L, 1);
+    size_t length = 0;
+    char const* const name = luaL_checklstring(L, 2, &length);
+    lua_Integer const base = luaL_checkinteger(L, 3);
+    void* const data = lua_touserdata(L, 4);
+    lua_Integer const size = luaL_checkinteger(L, 5);
+    lua_Integer const offset = luaL_optinteger(L, 6, 0);
+    int const readOnly = lua_toboolean(L, 7);
+
+    if (data == nullptr) {
+        return luaL_error(L, "data is null");
+    }
+
+    if (size <= 0) {
+        return luaL_error(L, "invalid size: %I", size);
+    }
+
+    if (offset < 0) {
+        return luaL_error(L, "invalid offset: %I", size);
+    }
+
+    self->_regions.emplace_back(std::string(name, length), data, offset, size, base, readOnly);
+
+    self->_logger->info(
+        "Added memory region {\"%s\", 0x%016" PRIxPTR ", 0x%016" PRIxPTR", %zu, %zu, %s}",
+        name, static_cast<uintptr_t>(base), reinterpret_cast<uintptr_t>(data), size, offset, readOnly ? "true" : "false"
+    );
+
+    return 0;
 }
--- src/Memory.h
+++ src/Memory.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Plugin.h"
 #include "Logger.h"
 
 #include <imgui_memory_editor.h>
@@ -7,26 +8,51 @@
 #include <string>
 #include <vector>
 
+extern "C" {
+    #include <lauxlib.h>
+}
+
 namespace hc {
-    class Memory {
+    class Memory : public Plugin {
     public:
         Memory() : _logger(nullptr), _selected(0) {}
 
-        bool init(Logger* logger);
-        void destroy();
-        void reset();
-        void draw();
+        void init(Logger* const logger);
 
-        void addRegion(char const* const name, void* data, size_t const base, size_t const size, bool const readOnly);
+        static Memory* check(lua_State* const L, int const index);
+
+        // hc::Plugin
+        virtual char const* getName() override;
+        virtual char const* getVersion() override;
+        virtual char const* getLicense() override;
+        virtual char const* getCopyright() override;
+        virtual char const* getUrl() override;
+
+        virtual void onStarted() override;
+        virtual void onConsoleLoaded() override;
+        virtual void onGameLoaded() override;
+        virtual void onGamePaused() override;
+        virtual void onGameResumed() override;
+        virtual void onGameReset() override;
+        virtual void onFrame() override;
+        virtual void onDraw() override;
+        virtual void onGameUnloaded() override;
+        virtual void onConsoleUnloaded() override;
+        virtual void onQuit() override;
+
+        virtual int push(lua_State* const L) override;
 
     protected:
+        static int l_addRegion(lua_State* const L);
+
         struct Region {
-            Region(char const* name, void* data, size_t base, size_t size, bool readOnly);
+            Region(std::string&& name, void* data, size_t offset, size_t size, size_t base, bool readOnly);
 
             std::string name;
             void* data;
-            size_t base;
+            size_t offset;
             size_t size;
+            size_t base;
             bool readOnly;
         };
 
