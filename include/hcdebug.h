2604ef907b4931ac766014b54b7dcb15ca54d954
Andre Leiradella <andre@leiradella.com>
2021-02-05 21:28:36 +0000

    Simplified the API; added CPU flags

--- include/hcdebug.h
+++ include/hcdebug.h
@@ -32,11 +32,12 @@ typedef struct {
     struct {
         char const* name;
         uint64_t flags;
-        uint64_t (*get)(hc_DebuggerIf const* debugger_if, unsigned index);
-        char const* const* bits;
+        uint64_t (*get)(void* user_data);
 
         /* set can be null if the register can't be changed or if it doesn't make sense to do so */ 
-        void (*set)(hc_DebuggerIf const* debugger_if, unsigned index, uint64_t value);
+        void (*set)(void* user_data, uint64_t value);
+
+        char const* const* bits;
     }
     v1;
 }
@@ -45,17 +46,17 @@ hc_Register;
 typedef struct {
     struct {
         char const* description;
+        uint64_t flags;
         uint64_t base_address;
         uint64_t size;
-        uint64_t flags;
-        uint8_t (*peek)(hc_DebuggerIf const* debugger_if, unsigned index, uint64_t address);
+        uint8_t (*peek)(void* user_data, uint64_t address);
 
         /* poke can be null for read-only memory but all memory should be writeable to allow patching */
         /* poke can be non-null and still don't change the value, i.e. for the main memory region when the address is in rom */
-        void (*poke)(hc_DebuggerIf const* debugger_if, unsigned index, uint64_t address, uint8_t value);
+        void (*poke)(void* user_data, uint64_t address, uint8_t value);
 
         /* set_watch_point can be null when not supported */
-        unsigned (*set_watch_point)(hc_DebuggerIf const* debugger_if, unsigned index, uint64_t address, uint64_t length, int read, int write);
+        unsigned (*set_watch_point)(void* user_data, uint64_t address, uint64_t length, int read, int write);
     }
     v1;
 }
@@ -66,22 +67,30 @@ typedef enum {
 }
 hc_CpuType;
 
+typedef enum {
+    HC_CPU_MAIN = 1 << 0
+}
+hc_CpuFlags;
+
 typedef struct {
     struct {
         hc_CpuType type;
         char const* description;
+        uint64_t flags;
         hc_Register const* const* registers;
         unsigned num_registers;
         hc_Memory const* const* memory_regions;
         unsigned num_memory_regions;
 
-        /* these can be null if the cpu doesn't support debugging; the main cpu must support them */
-        void (*pause)(hc_DebuggerIf const* debugger_if, unsigned index);
-        void (*resume)(hc_DebuggerIf const* debugger_if, unsigned index);
-        void (*step)(hc_DebuggerIf const* debugger_if, unsigned index);
+        /* any one of these can be null if the cpu doesn't support the functionality */
+        void (*pause)(void* user_data);
+        void (*resume)(void* user_data);
+        void (*step_into)(void* user_data);
+        void (*step_over)(void* user_data);
+        void (*step_out)(void* user_data);
 
         /* set_break_point can be null when not supported */
-        unsigned (*set_break_point)(hc_DebuggerIf const* debugger_if, unsigned index, uint64_t address);
+        unsigned (*set_break_point)(void* user_data, uint64_t address);
     }
     v1;
 }
@@ -110,6 +119,6 @@ struct hc_DebuggerIf {
     v1;
 };
 
-typedef void (*hc_Set)(hc_DebuggerIf* const debugger_if);
+typedef void* (*hc_Set)(hc_DebuggerIf* const debugger_if);
 
 #endif /* HC_DEBUG__ */
