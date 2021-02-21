20d97dc3a29f8db6dd250c3d4f9f911a5403c7fb
Andre Leiradella <andre@leiradella.com>
2021-02-21 15:53:02 +0000

    Made autorun.lua recurse into subfolders

--- etc/scripts/autorun.lua
+++ etc/scripts/autorun.lua
@@ -3,23 +3,47 @@ local lfs = require 'lfs'
 
 local logger, config = hc.logger, hc.config
 
-local scriptsPath = config:getScriptsPath()
-logger:info('Looking for Lua scripts in "%s"', scriptsPath)
+local function run(file)
+    local func, err = loadfile(file, 't')
 
-for file in lfs.dir(scriptsPath) do
-    if file:sub(-4, -1) == '.lua' and file ~= 'autorun.lua' then
-        logger:info('Found "%s", running...', file)
+    if not func then
+        logger:error('%s', err)
+    else
+        local ok, err = pcall(func)
 
-        local func, err = loadfile(scriptsPath .. file, 't')
-
-        if not func then
+        if not ok then
             logger:error('%s', err)
-        else
-            local ok, err = pcall(func)
-
-            if not ok then
-                logger:error('%s', err)
-            end
         end
+    end
+end
+
+local function recurse(path)
+    logger:info('Recursing into %s', path)
+
+    for file in lfs.dir(path) do
+        local filePath = path .. '/' .. file
+        local mode = lfs.attributes(filePath, 'mode')
+
+        if mode == 'directory' and file ~= '.' and file ~= '..' then
+            recurse(filePath)
+        elseif mode == 'file' and file:sub(-4, -1) == '.lua' then
+            logger:info('Found "%s", running...', filePath)
+            run(filePath)
+        end
+    end
+end
+
+local path = config:getScriptsPath()
+logger:info('Looking for Lua scripts in "%s"', path)
+
+for file in lfs.dir(path) do
+    local filePath = path .. '/' .. file
+    local mode = lfs.attributes(filePath, 'mode')
+
+    if mode == 'directory' and file ~= '.' and file ~= '..' then
+        recurse(filePath)
+    elseif mode == 'file' and file:sub(-4, -1) == '.lua' and file ~= 'autorun.lua' then
+        logger:info('Found "%s", running...', filePath)
+        run(filePath)
     end
 end
