dbc7c50ee07048dec8674f7070024732420da18d
Andre Leiradella <andre@leiradella.com>
2021-05-12 08:59:33 +0100

    Don't register the os module

--- src/Application.cpp
+++ src/Application.cpp
@@ -280,7 +280,25 @@ bool hc::Application::init(std::string const& title, int const width, int const 
 
         undo.add([this]() { lua_close(_L); });
 
-        luaL_openlibs(_L);
+        static luaL_Reg const libs[] = {
+            {LUA_GNAME, luaopen_base},
+            {LUA_LOADLIBNAME, luaopen_package},
+            {LUA_COLIBNAME, luaopen_coroutine},
+            {LUA_TABLIBNAME, luaopen_table},
+            {LUA_IOLIBNAME, luaopen_io},
+            //{LUA_OSLIBNAME, luaopen_os},
+            {LUA_STRLIBNAME, luaopen_string},
+            {LUA_MATHLIBNAME, luaopen_math},
+            {LUA_UTF8LIBNAME, luaopen_utf8},
+            {LUA_DBLIBNAME, luaopen_debug},
+            {NULL, NULL}
+        };
+
+        for (luaL_Reg const* lib = libs; lib->func != nullptr; lib++) {
+            luaL_requiref(_L, lib->name, lib->func, 1);
+            lua_pop(_L, 1);
+        }
+
         push(_L);
         lua_pushvalue(_L, -1);
         lua_setglobal(_L, "hc");
