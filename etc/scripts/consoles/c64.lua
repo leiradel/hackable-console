ac9aff9571be539d0a03c1b052d892cd6e67a52d
Andre Leiradella <andre@leiradella.com>
2021-07-09 18:44:41 +0100

    Added both Vice cores

--- etc/scripts/consoles/c64.lua
+++ etc/scripts/consoles/c64.lua
@@ -6,3 +6,45 @@ hc.control:addConsole('Commodore 64 (C64 Chips)', {
         hc.control:loadCore(path)
     end
 })
+
+hc.control:addConsole('Commodore 64 (Vice Fast)', {
+    onConsoleLoaded = function()
+        local path = hc.config:getCoresPath() .. 'vice_x64_libretro.' .. hc.soExtension
+        hc.control:loadCore(path)
+    end,
+
+    onGameLoaded = function()
+        local map = hc.config:getMemoryMap()
+        local blocks = {}
+
+        hc.logger:info('Adding memory region for System RAM')
+
+        for _, desc in pairs(map) do
+            blocks[#blocks + 1] = {desc.pointer, desc.start, desc.length}
+            hc.logger:info('    %p, %x, %d', desc.pointer, desc.start, desc.length)
+        end
+
+        hc.config:addMemory('sram', 'System RAM', false, table.unpack(blocks))
+    end
+})
+
+hc.control:addConsole('Commodore 64 (Vice Accurate)', {
+    onConsoleLoaded = function()
+        local path = hc.config:getCoresPath() .. 'vice_x64sc_libretro.' .. hc.soExtension
+        hc.control:loadCore(path)
+    end,
+
+    onGameLoaded = function()
+        local map = hc.config:getMemoryMap()
+        local blocks = {}
+
+        hc.logger:info('Adding memory region for System RAM')
+
+        for _, desc in pairs(map) do
+            blocks[#blocks + 1] = {desc.pointer, desc.start, desc.length}
+            hc.logger:info('    %p, %x, %d', desc.pointer, desc.start, desc.length)
+        end
+
+        hc.config:addMemory('sram', 'System RAM', false, table.unpack(blocks))
+    end
+})
